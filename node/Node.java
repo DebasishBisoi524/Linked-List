823ded91df13a343a35bb24a727ad35a23f1f5ad
DebasishBisoi524 <debaishbisoi524@gmail.com>
2025-04-03 16:40:37 +0530

    changes are made

--- node/Node.java
+++ node/Node.java
@@ -1,25 +1,51 @@
 package node;
-public class Node{
-    private int data;
-    private Node next;
 
-    public Node(int data){
-        this.data = data;
+/**
+ * Represents a node in a singly linked list.
+ * The node's data is immutable after creation, while the next reference remains mutable
+ * to allow for list manipulation.
+ */
+public class Node {
+    private final int data;  // Immutable data field
+    private Node next;       // Mutable next reference
+
+    /**
+     * Constructs a node with the specified data and null next reference.
+     * @param data The integer value to store in this node
+     */
+    public Node(int data) {
+        this(data, null);
     }
 
-    public int getData(){
+    /**
+     * Constructs a node with the specified data and next reference.
+     * @param data The integer value to store in this node
+     * @param next The next node in the list
+     */
+    public Node(int data, Node next) {
+        this.data = data;
+        this.next = next;
+    }
+
+    /**
+     * @return The data contained in this node
+     */
+    public int getData() {
         return data;
     }
 
-    public void setData(int data){
-        this.data = data;
-    }
-
-    public Node getNext(){
+    /**
+     * @return The next node in the list
+     */
+    public Node getNext() {
         return next;
     }
 
-    public void setNext(Node next){
+    /**
+     * Sets the next node reference
+     * @param next The node that should follow this one in the list
+     */
+    public void setNext(Node next) {
         this.next = next;
     }
 }
